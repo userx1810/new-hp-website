b42fda1333ce83367c6ca1e89f2b459b67aa0b92
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-05-03 15:56:27 +0200

    .

--- front/js/register.js
+++ front/js/register.js
@@ -2,33 +2,52 @@ const formulaire = document.getElementById("formulaire");
 
 formulaire.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const response = await fetch("http://localhost:3000/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
+  try {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-  const data = await response.json();
-
-  console.log(data);
-  window.location.href = "/index.html";
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      // Assuming the server returns a token upon successful login
+      localStorage.setItem("token", data.token);
+      window.location.href = "index.html";
+    } else {
+      // Handle error response
+      console.error("Login failed");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 });
 
 const getMyProfile = async () => {
   const token = localStorage.getItem("token");
 
-  const response = await fetch("http://localhost:3000/getMyProfile", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  try {
+    const response = await fetch("http://localhost:3000/getMyProfile", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const data = await response.json();
-
-  console.log(data);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      // Handle error response
+      console.error("Failed to fetch profile data");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 };
