f38930ce649b82124dd0f24dde80f034e825bb1f
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-05-10 16:21:11 +0200

    Update accueil.js

--- front/js/accueil.js
+++ front/js/accueil.js
@@ -1,11 +1,10 @@
-"use strict"; // Utilise le mode strict pour des contrôles supplémentaires
+"use strict";
 
-const menuIcon = document.getElementById("bx-menu"); // Récupère l'icône du menu dans le DOM
-const menu = document.getElementById("menu"); // Récupère le menu dans le DOM
-const sidebar = document.querySelector(".sidebar"); // Récupère la barre latérale dans le DOM
+const menuIcon = document.getElementById("bx-menu");
+const menu = document.getElementById("menu");
+const sidebar = document.querySelector(".sidebar");
 
 menuIcon.addEventListener("click", () => {
-  // Ajoute un écouteur d'événements au clic sur l'icône du menu
-  menu.classList.toggle("active"); // Ajoute ou supprime la classe 'active' du menu
-  sidebar.classList.toggle("active"); // Ajoute ou supprime la classe 'active' de la barre latérale
+  menu.classList.toggle("active");
+  sidebar.classList.toggle("active");
 });
