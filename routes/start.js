82326045cbe499a575e1e0ba16bbb7196cb7564c
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-06-30 15:58:25 +0200

    .

---
+++ routes/start.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const usersController = require("../controllers/UsersController");
+const authController = require("../controllers/AuthentificationController");
+const authMiddleware = require("../middlewares/auth");
+
+const router = express.Router();
+
+router.get("/users", usersController.index);
+router.post("/users", usersController.store);
+router.get("/users/:id", usersController.show);
+router.put("/users/:id", usersController.update);
+router.delete("/users/:id", usersController.destroy);
+
+router.post("/login", authController.login);
+
+router.get(
+  "/getMyProfile",
+  authMiddleware.authenticate,
+  usersController.getMyProfile,
+);
+
+module.exports = router;
